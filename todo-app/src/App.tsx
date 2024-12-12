5b4f7f3f5bde6f15bd403818af9805aae1461e80
DaniilRai13 <testnet02@mail.ru>
2024-12-12 23:27:58 +0300

    feat: create routes to different pages: App and Auth

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
+import { Route, Routes } from 'react-router'
+import './App.scss'
+import Layout from './components/layout/Layout'
+import Calendar from './components/pages/Calendar/Calendar'
+import Profile from './components/pages/Profile/Profile'
+import Home from './components/screens/Home'
+import { pageRoutes } from './config/pageRoutes'
 
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route
+        path={pageRoutes.home}
+        element={<Layout />}
+      >
+        <Route path='dashboard' element={<Home />} />
+        <Route path='calendar' element={<Calendar />} />
+        <Route path='profile' element={<Profile />} />
+      </Route>
+    </Routes>
   )
 }
+
+
 
 export default App
