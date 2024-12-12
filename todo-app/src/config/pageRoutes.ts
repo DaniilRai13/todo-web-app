a457f40acc0591f4111420914d0b12fd9cf38be1
DaniilRai13 <testnet02@mail.ru>
2024-12-12 23:32:35 +0300

    feat: create pages routes and add type for lucide icons

---
+++ todo-app/src/config/pageRoutes.ts
@@ -0,0 +1,45 @@
+import { icons } from 'lucide-react'
+
+interface IPageRoutes {
+	home: string
+	auth: string
+}
+
+export const pageRoutes: IPageRoutes = {
+	home: '/',
+	auth: 'auth'
+}
+
+export interface INavigateSideProps {
+	name: string,
+	icon: IconNames,
+	color: string
+	link: string
+}
+type IconNames = keyof typeof icons
+export const navigateSideProps: INavigateSideProps[] = [
+	{
+		name: 'Overview',
+		icon: 'LayoutGrid',
+		color: '#59b081',
+		link: '/overview'
+	},
+	{
+		name: 'Todos',
+		icon: 'NotebookPen',
+		color: '#c84848',
+		link: '/todo-list'
+	},
+	{
+		name: 'Calendar',
+		icon: 'CalendarDays',
+		color: '#b46cd1',
+		link: '/calendar'
+	},
+	{
+		name: 'Profile',
+		icon: 'UserRoundPen',
+		color: '#F59E0B',
+		link: '/profile'
+	},
+]
