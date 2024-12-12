cfcd3a5e4369fd3fc5257511487d413e165a1908
DaniilRai13 <testnet02@mail.ru>
2024-12-12 23:30:05 +0300

    feat: add to different routes: to app and to auth page

--- todo-app/src/main.tsx
+++ todo-app/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './App.tsx'
+import Auth from './components/screens/Auth.tsx'
+import { pageRoutes } from './config/pageRoutes.ts'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={pageRoutes.home + '*'}
+          element={<App />}
+        />
+        <Route
+          path={pageRoutes.auth}
+          element={<Auth />}
+        />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
