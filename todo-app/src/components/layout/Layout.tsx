815609c5d618d746e6814323bc30c8821c3fa0ee
DaniilRai13 <testnet02@mail.ru>
2024-12-13 16:45:40 +0300

    feat: add MainHeader component

--- todo-app/src/components/layout/Layout.tsx
+++ todo-app/src/components/layout/Layout.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react'
 import Navigation from './Navigation/Navigation'
 import { Outlet } from 'react-router'
 import styles from './Layout.module.scss'
+import MainHeader from './MainHeader/MainHeader'
 const Layout: FC = () => {
 	return (
 		<div className={styles.layout}>
 			<Navigation />
 			<main className={styles.main}>
+				<MainHeader />
 				<Outlet />
 			</main>
 		</div>
