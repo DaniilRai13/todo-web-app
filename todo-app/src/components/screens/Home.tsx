0a68b6baf6b81ffa8327fa8386eb0c6c9c85982d
DaniilRai13 <testnet02@mail.ru>
2024-12-22 23:00:33 +0300

    feat: create empty component

---
+++ todo-app/src/components/screens/Home.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+const Home: FC = () => {
+	return (
+		<div>
+			<section className='aaa1'></section>
+			<section className='aaa2'></section>
+			<section className='aaa3'></section>
+			<section className='aaa4'></section>
+		</div>
+	)
+}
+
+export default Home
