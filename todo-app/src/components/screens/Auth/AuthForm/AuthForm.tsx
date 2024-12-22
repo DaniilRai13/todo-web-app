69b6cfd2846a466a250af0b09c6ea9329c53b6dc
DaniilRai13 <testnet02@mail.ru>
2024-12-22 23:02:34 +0300

    feat: use useForm for form and add custom fields component

--- todo-app/src/components/screens/Auth/AuthForm/AuthForm.tsx
+++ todo-app/src/components/screens/Auth/AuthForm/AuthForm.tsx
@@ -1,12 +1,22 @@
 import { FC } from 'react'
 // import { useForm } from 'react-hook-form'
 import cn from 'classnames'
+import { useForm } from 'react-hook-form'
 import { NavLink } from 'react-router'
 import Button from '../../../../shared/Button/Button'
+import Field from '../../../../shared/form/Field'
+import { IField } from '../../../../shared/form/form.interface'
 import styles from './AuthForm.module.scss'
 
 const AuthForm: FC<{ name: string, isPage: boolean }> = ({ name, isPage }) => {
-	// const { register, handleSubmit, formState } = useForm()
+	const { register, handleSubmit, formState: { errors } } = useForm<IField>({
+		mode: 'onChange'
+	})
+	const { isLoading } = useTypedSelector(({ user }) => user)
+	const onSubmit = (data: IField) => {
+		console.log(data)
+	}
+
 	return (
 		<div className={styles.formWindow}>
 			<h1
@@ -14,24 +24,32 @@ const AuthForm: FC<{ name: string, isPage: boolean }> = ({ name, isPage }) => {
 					[styles.register]: isPage,
 					[styles.login]: !isPage
 				})}>{name.toUpperCase()}</h1>
-			<form className={styles.form}>
-				<label>
-					<span>Email:</span>
-					<input
-						className={cn(styles.input, {
-							[styles.register]: isPage,
-							[styles.login]: !isPage
-						})} />
-				</label>
-				<label>
-					<span>Password:</span>
-					<input
-						type='password'
-						className={cn(styles.input, {
-							[styles.register]: isPage,
-							[styles.login]: !isPage
-						})} />
-				</label>
+			<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+				<Field
+					{...register('email', {
+						required: 'This field is required',
+						pattern: {
+							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+							message: 'Invalid email address'
+						}
+					})}
+					placeholder='Email'
+					isPage={isPage}
+					error={errors.email}
+				/>
+				<Field
+					{...register('password', {
+						required: 'This field is required',
+						minLength: {
+							value: 6,
+							message: 'Password must be at least 6 characters',
+						},
+					})}
+					placeholder='Password'
+					type='password'
+					isPage={isPage}
+					error={errors.password}
+				/>
 				<Button
 					title={name.toUpperCase()}
 					classNames={isPage ? styles.btn_register : styles.btn_login}
