6cad626e8dfa0edf5d5ff22c13c931c37f1a1630
DaniilRai13 <testnet02@mail.ru>
2024-12-22 23:03:21 +0300

    feat: connect my project with firestore

---
+++ todo-app/src/config/firestore.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCe6m67zARBHuxAEfOUT_509tanuWhLWdM",
+  authDomain: "todo-app-ddb2d.firebaseapp.com",
+  projectId: "todo-app-ddb2d",
+  storageBucket: "todo-app-ddb2d.firebasestorage.app",
+  messagingSenderId: "13133991044",
+  appId: "1:13133991044:web:ff1e52347b3f04dddb06d1",
+  measurementId: "G-V0JVPJLQLE"
+}
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig)
+export const auth = getAuth(firebaseApp)
