ad7694953c296d035631bdaf83659094aac25389
DaniilRai13 <danik.ray.23@mail.ru>
2024-12-23 19:18:30 +0300

    fix: update error type in IFieldProps for better compatibility with react-hook-form

--- todo-app/src/shared/form/form.interface.ts
+++ todo-app/src/shared/form/form.interface.ts
@@ -1,9 +1,9 @@
 import { InputHTMLAttributes } from 'react'
-import { FieldError } from 'react-hook-form'
+import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form'
 
 export interface IFieldProps {
 	placeholder: string,
-	error?: FieldError | undefined
+	error?: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined
 }
 
 type TypeInputPropsField = InputHTMLAttributes<HTMLInputElement> & IFieldProps
