c4efd958e40b3d862df0dd20b585bec6d701cd77
DaniilRai13 <danik.ray.23@mail.ru>
2024-12-25 16:31:10 +0300

    feat: add exit button and display user data

--- todo-app/src/components/layout/MainHeader/MainHeader.tsx
+++ todo-app/src/components/layout/MainHeader/MainHeader.tsx
@@ -1,18 +1,29 @@
 import { FC } from 'react'
 import NewTaskButton from '../../../shared/create-task-button/NewTaskButton'
 import styles from './MainHeader.module.scss'
+import { useTypedSelector } from '../../../shared/hooks/useTypedSelector'
+import { Icon } from '../../../shared/LucidIcon'
+import { useActions } from '../../../shared/hooks/useActions'
+
 const MainHeader: FC = () => {
+	const { user } = useTypedSelector(({ user }) => user)
+	const { logout } = useActions()
+	
 	return (
 		<header className={styles.header}>
 			<NewTaskButton />
 			<div className={styles.profile}>
+				<div className={styles.exit} onClick={logout}>
+					<Icon icon='LogOut' color='white' />
+					<h3>Log</h3>
+				</div>
 				<div className={styles.profileImg}><img alt='image' /></div>
 				<div className={styles.info}>
-					<h4 className={styles.name}>
-						Daniil Rai
-					</h4>
+					<h3 className={styles.name}>
+						{user?.name ? user.name : 'User'}
+					</h3>
 					<h5 className={styles.email}>
-						testnet02@mail.ru
+						{user?.email}
 					</h5>
 				</div>
 			</div>
