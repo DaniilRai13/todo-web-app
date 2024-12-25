83ae454346f50477e8e5ad4c3113ef475bdc83e3
DaniilRai13 <danik.ray.23@mail.ru>
2024-12-25 16:37:57 +0300

    feat: handling isLoading status

--- todo-app/src/shared/Button/Button.tsx
+++ todo-app/src/shared/Button/Button.tsx
@@ -4,11 +4,12 @@ import cn from 'classnames'
 interface IButton {
 	title: string
 	classNames?: string
+	isLoading?: boolean
 	onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
-const Button: FC<IButton> = ({ title, classNames, onClick }) => {
+const Button: FC<IButton> = ({ title, classNames, onClick, isLoading }) => {
 	return (
-		<button className={cn(styles.button, classNames)} onClick={onClick}>{title}</button>
+		<button className={cn(styles.button, classNames)} onClick={onClick}>{isLoading ? 'Loading...' : title}</button>
 	)
 }
 
