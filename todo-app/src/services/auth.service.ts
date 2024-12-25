e5c8ec947ce7e010c17547e5864725003e81af7f
DaniilRai13 <danik.ray.23@mail.ru>
2024-12-25 16:37:22 +0300

    feat: add login, logout and checkTokenns methods and handling errors in register method

--- todo-app/src/services/auth.service.ts
+++ todo-app/src/services/auth.service.ts
@@ -1,19 +1,64 @@
-import { browserSessionPersistence, createUserWithEmailAndPassword, setPersistence, signInWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 import { IAuthData } from '../interfaces/Auth.interface'
 import { auth } from '../config/firestore'
+import { IProfileData } from "../config/user.data";
+import { removeTokensStorage, saveToStorage } from "./auth.helper";
+
 
 export const AuthService = {
-	register: ({ email, password }: IAuthData) => {
-		createUserWithEmailAndPassword(auth, email, password)
+	register: async ({ email, password }: IAuthData): Promise<{ message: string }> => {
+		try {
+			await createUserWithEmailAndPassword(auth, email, password);
+			return { message: 'User registered successfully' };
+		} catch (error: any) {
+			if (error.code === 'auth/email-already-in-use') {
+				throw new Error('This email is already in use. Please use a different one.');
+			}
+			if (error.code === 'auth/invalid-email') {
+				throw new Error('Invalid email address. Please enter a valid email.');
+			}
+			if (error.code === 'auth/weak-password') {
+				throw new Error('Password should be at least 6 characters long.');
+			}
+			if (error.code === 'auth/missing-email' || error.code === 'auth/missing-password') {
+				throw new Error('Please enter both email and password.');
+			}
+			throw new Error('Registration failed. Please try again later.');
+		}
 	},
-	login: ({ email, password }: IAuthData) => {
-		setPersistence(auth, browserSessionPersistence)
-			.then(() => {
-				return signInWithEmailAndPassword(auth, email, password)
-			})
-			.catch((error) => {
-				// Handle Errors here.
-				console.log(error)
-			})
+	login: async ({ email: loginEmail, password }: IAuthData): Promise<IProfileData> => {
+		try {
+			const userCredential = await signInWithEmailAndPassword(auth, loginEmail, password);
+			const user = userCredential.user;
+
+			const {
+				displayName,
+				email,
+				refreshToken,
+				photoURL,
+				uid,
+				metadata: { creationTime }
+			} = user
+			const accessToken = await user.getIdToken()
+			saveToStorage({ refreshToken, accessToken })
+
+			return {
+				id: uid,
+				name: displayName,
+				email,
+				img: photoURL,
+				createdAt: creationTime
+			};
+
+		} catch (error) {
+			console.error('Error during login:', error);
+			throw error;
+		}
+	},
+	logout: () => {
+		removeTokensStorage()
+	},
+	checkTokens: () => {
+		return localStorage.getItem('user')
 	}
 }
