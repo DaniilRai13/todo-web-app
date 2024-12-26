6ceebb405e7a6a32032cc0d560e3395a55624efc
DaniilRai13 <danik.ray.23@mail.ru>
2024-12-26 17:21:57 +0300

    feat: add new action: resetStatus

--- todo-app/src/shared/hooks/useActions.ts
+++ todo-app/src/shared/hooks/useActions.ts
@@ -1,9 +1,11 @@
 import { bindActionCreators } from '@reduxjs/toolkit'
 import { useMemo } from 'react'
 import { useDispatch } from 'react-redux'
+import { resetStatus } from '../../store/user/userSlice.ts'
 import * as userActions from '../../store/user/user.actions.ts'
 
 const rootActions = {
+	resetStatus,
 	...userActions
 }
 
