12f4a3640b5867db2206045f0848e85a668a51fb
DaniilRai13 <danik.ray.23@mail.ru>
2024-12-27 14:10:33 +0300

    refactor: refactor getUserFromStorage for safe JSON parsing

--- todo-app/src/store/user/user.localstorage.ts
+++ todo-app/src/store/user/user.localstorage.ts
@@ -3,8 +3,14 @@ import { IProfileData } from "../../config/user.data"
 export const saveUserToStorage = (data: IProfileData) => {
   localStorage.setItem('user', JSON.stringify(data))
 }
-export const getUserFromStorage = () => {
-  return localStorage.getItem('user')
+export const getUserFromStorage = (): IProfileData | null => {
+  const user = localStorage.getItem('user');
+
+  if (user) {
+    return JSON.parse(user) as IProfileData;
+  }
+
+  return null;
 }
 export const removeUserStorage = () => {
   localStorage.removeItem('user')
