3851be6488069af07496ca7f12bc17f3a2731b53
DaniilRai13 <danik.ray.23@mail.ru>
2024-12-29 00:25:24 +0300

    feat: handle window width and then change a margin for main block

--- todo-app/src/components/layout/Layout.tsx
+++ todo-app/src/components/layout/Layout.tsx
@@ -1,16 +1,39 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import Navigation from './Navigation/Navigation'
 import { Outlet } from 'react-router'
 import styles from './Layout.module.scss'
 import MainHeader from './MainHeader/MainHeader'
+import { motion } from 'framer-motion'
+import useAnimation from './useAnimation'
+
 const Layout: FC = () => {
+	const [isOpen, setIsOpen] = useState(true)
+	const { setWindowWidth, getAsideWidth } = useAnimation({ isOpen })
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth)
+		}
+		window.addEventListener('resize', handleResize)
+
+		return () => {
+			window.removeEventListener('resize', handleResize)
+		}
+	}, [])
+
 	return (
-		<div className={styles.layout}>
-			<Navigation />
-			<main className={styles.main}>
-				<MainHeader />
-				<Outlet />
-			</main>
+		<div className={styles.container}>
+			<div className={styles.layout}>
+				<Navigation isOpen={isOpen} setIsOpen={setIsOpen} />
+				<motion.main
+					className={styles.main}
+					initial={{ marginLeft: getAsideWidth() }}
+					animate={{ marginLeft: getAsideWidth() }}
+				>
+					<MainHeader />
+					<Outlet />
+				</motion.main>
+			</div>
 		</div>
 	)
 }
