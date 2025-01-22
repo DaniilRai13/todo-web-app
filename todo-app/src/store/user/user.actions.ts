759c364a20b90b9e9a62dd565581ba69be50569a
DaniilRai13 <danik.ray.23@mail.ru>
2025-01-23 01:45:36 +0300

    feat: create new actions as 'update profile' and 'update password'

--- todo-app/src/store/user/user.actions.ts
+++ todo-app/src/store/user/user.actions.ts
@@ -1,6 +1,8 @@
+import { IProfileData } from './../../config/user.data';
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { IAuthData } from '../../interfaces/Auth.interface'
 import { AuthService } from '../../services/auth.service'
+import { userService } from '../../services/user.service'
 
 export const register = createAsyncThunk(
 	'user/register',
@@ -22,5 +24,29 @@ export const logout = createAsyncThunk(
 	'user/logout',
 	async () => {
 		return AuthService.logout()
+	}
+)
+
+export const updateProfile = createAsyncThunk<IProfileData, { id: string, data: IProfileData }>(
+	'user/updateProfile',
+	async ({ id, data }) => {
+		try {
+			await userService.updateProfile(id, data)
+			return data
+		} catch (error: any) {
+			console.error(error)
+			throw new Error(error)
+		}
+	}
+)
+export const updateUserPassword = createAsyncThunk<void, { password: string, newPassword: string }>(
+	'user/updatePassword',
+	async ({ password, newPassword }) => {
+		try {
+			await userService.updateUserPassword(password, newPassword)
+		} catch (error: any) {
+			console.error(error)
+			throw new Error(error)
+		}
 	}
 )
