b3a6d01f4e722e711c4ac5b5a300df1c180cd259
DaniilRai13 <danik.ray.23@mail.ru>
2025-01-27 00:38:20 +0300

    feat: add the ability to add an icon

--- todo-app/src/shared/Button/Button.tsx
+++ todo-app/src/shared/Button/Button.tsx
@@ -1,19 +1,33 @@
 import { ButtonHTMLAttributes, FC } from 'react'
 import styles from './Button.module.scss'
 import cn from 'classnames'
+import { Icon } from '../LucidIcon'
+import { IconNames } from '../../config/pageRoutes'
+
 interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
 	title: string
 	classNames?: string
 	isLoading?: boolean
+	icon?: IconNames,
+	size?: number,
+	color?: string,
 	onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
-const Button: FC<IButton> = ({ title, classNames, onClick, isLoading, ...props }) => {
+
+const Button: FC<IButton> = ({ title, icon, size, color, classNames, onClick, isLoading, ...props }) => {
 	return (
 		<button
 			className={cn(styles.button, classNames)}
 			onClick={onClick}
 			{...props}
-		>{isLoading ? 'Loading...' : title}</button>
+		>
+			{icon && <Icon
+				icon={icon}
+				size={size}
+				color={color}
+			/>}
+			{isLoading ? <h3>Loading...</h3> : <h3>{title}</h3>}
+		</button>
 	)
 }
 
