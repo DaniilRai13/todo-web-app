bcc7694351065a444ad8963723910474a9984eb7
DaniilRai13 <testnet02@mail.ru>
2025-02-13 20:30:55 +0300

    feat: add icon to create task button and navigate to /profile page

--- todo-app/src/components/layout/MainHeader/MainHeader.tsx
+++ todo-app/src/components/layout/MainHeader/MainHeader.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react'
-import styles from './MainHeader.module.scss'
+import { NavLink } from 'react-router'
+import { useModalActions } from '../../../provider/ModalProvider'
+import Button from '../../../shared/Button/Button'
+import { useActions } from '../../../shared/hooks/useActions'
 import { useTypedSelector } from '../../../shared/hooks/useTypedSelector'
 import { Icon } from '../../../shared/LucidIcon'
-import { useActions } from '../../../shared/hooks/useActions'
-import Button from '../../../shared/Button/Button'
-import { useModalActions } from '../../../provider/ModalProvider'
 import CreateTaskModal from '../../../shared/Modal/CreateTaskModal'
+import styles from './MainHeader.module.scss'
 
 const MainHeader: FC = () => {
 	const { user } = useTypedSelector(({ user }) => user)
@@ -14,32 +15,39 @@ const MainHeader: FC = () => {
 	return (
 		<header className={styles.header}>
 			<Button
-				title='New Task'
+				title='New'
 				icon='Plus'
 				size={20}
 				color='black'
 				style={{ color: 'black' }}
 				onClick={() => setOpen(true)}
-			/>
+			><Icon
+					icon={'NotebookPen'}
+					size={20}
+					color={'#000'}
+					style={{ marginLeft: '10px', marginRight: '10px' }}
+				/></Button>
 			<CreateTaskModal />
 			<div className={styles.profile}>
 				<div className={styles.exit} onClick={logout}>
 					<Icon icon='LogOut' color='black' size={16} />
 					<h3>Log</h3>
 				</div>
-				<div className={styles.profileImg}>
-					<img alt='image' src={user?.img || ''} />
-				</div>
-				<div className={styles.info}>
-					<h3 className={styles.name}>
-						{user?.username ? user.username : 'User'}
-					</h3>
-					<h5 className={styles.email}>
-						{user?.email}
-					</h5>
-				</div>
+				<NavLink to='/profile' className={styles.profileInfo}>
+					<div className={styles.profileImg}>
+						<img alt='image' src={user?.img || ''} />
+					</div>
+					<div className={styles.info}>
+						<h3 className={styles.name}>
+							{user?.username ? user.username : 'User'}
+						</h3>
+						<h5 className={styles.email}>
+							{user?.email}
+						</h5>
+					</div>
+				</NavLink>
 			</div>
-		</header>
+		</header >
 	)
 }
 
