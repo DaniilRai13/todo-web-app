e82516288c2530330e6c1aacdf7c8b766ef37974
DaniilRai13 <testnet02@mail.ru>
2025-02-13 20:32:25 +0300

    refactor: error handling and change the task data in request

--- todo-app/src/services/tasksService/tasks.service.ts
+++ todo-app/src/services/tasksService/tasks.service.ts
@@ -1,20 +1,22 @@
-import { doc, setDoc } from "firebase/firestore";
-import { db } from "../../config/firestore";
-import { ITask } from "../../config/user.data";
+import { doc, setDoc } from "firebase/firestore"
+import { db } from "../../config/firestore"
+import { ITask } from "../../config/user.data"
+import { FirebaseError } from 'firebase/app'
 
 export const tasksService = {
-  create: async (id: string, data: ITask): Promise<ITask> => {
+  create: async (userId: string, task: ITask): Promise<ITask> => {
     try {
-      const taskRef = doc(db, "user", id, 'tasks', data.id)
-      await setDoc(taskRef, data)
-      return data;
-    } catch (error) {
+      const taskRef = doc(db, "users", userId, 'tasks', task.uid)
+      await setDoc(taskRef, task)
+      return task
+    } catch (error: unknown) {
+      if (error instanceof FirebaseError) {
+        throw new Error("Task dosn't be created")
+      }
       throw new Error("Task dosn't be created")
     }
   },
   get: () => { },
   update: () => { },
-  delete: () => { },
-  sort: () => { },
-  find: () => { }
+  delete: () => { }
 }
