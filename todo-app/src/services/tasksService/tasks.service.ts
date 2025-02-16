722a78287dadb2935a88c024d6bd34da18bb7e7b
DaniilRai13 <testnet02@mail.ru>
2025-02-16 20:01:05 +0300

    feat: create new method to get tasks

--- todo-app/src/services/tasksService/tasks.service.ts
+++ todo-app/src/services/tasksService/tasks.service.ts
@@ -1,7 +1,7 @@
-import { doc, setDoc } from "firebase/firestore"
+import { FirebaseError } from 'firebase/app'
+import { collection, doc, getDocs, setDoc } from "firebase/firestore"
 import { db } from "../../config/firestore"
 import { ITask } from "../../config/user.data"
-import { FirebaseError } from 'firebase/app'
 
 export const tasksService = {
   create: async (userId: string, task: ITask): Promise<ITask> => {
@@ -16,7 +16,35 @@ export const tasksService = {
       throw new Error("Task dosn't be created")
     }
   },
-  get: () => { },
+  get: async (userId: string) => {
+    try {
+      const tasksRef = collection(db, "users", userId, 'tasks')
+      const querySnapshot = await getDocs(tasksRef)
+      const tasks = querySnapshot.docs.map(doc => {
+        const data = doc.data()
+
+        return {
+          uid: data.uid,
+          title: data.title,
+          description: data.description,
+          status: data.status,
+          startDate: data.startDate,
+          endDate: data.endDate,
+          priority: data.priority,
+          createdAt: data.createdAt,
+          updatedAt: data.updatedAt
+        }
+      })
+
+      return tasks
+    } catch (error: unknown) {
+      console.error(error)
+      if (error instanceof FirebaseError) {
+        throw new Error("Tasks were not received")
+      }
+      throw new Error("Tasks were not received")
+    }
+  },
   update: () => { },
   delete: () => { }
 }
