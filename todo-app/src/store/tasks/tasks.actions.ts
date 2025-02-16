71ea24e68dd4b04a17a339a68fe46d33b07ce05e
DaniilRai13 <testnet02@mail.ru>
2025-02-16 20:00:02 +0300

    feat: create action to get tasks

--- todo-app/src/store/tasks/tasks.actions.ts
+++ todo-app/src/store/tasks/tasks.actions.ts
@@ -7,13 +7,23 @@ interface ITaskProps {
   userId: string
 }
 
-
-
 export const createTask = createAsyncThunk(
   'task/create',
   async ({ task, userId }: ITaskProps, { rejectWithValue }) => {
     try {
       const response = await tasksService.create(userId, task)
+      return response
+    } catch (error: unknown) {
+      const errorEdit = error instanceof Error ? error.message : 'An error occurred'
+      return rejectWithValue(errorEdit)
+    }
+  }
+)
+export const getTasks = createAsyncThunk<ITask[], string, { rejectValue: string }>(
+  'tasks/get',
+  async (userId: string, { rejectWithValue }) => {
+    try {
+      const response = await tasksService.get(userId)
       return response
     } catch (error: unknown) {
       const errorEdit = error instanceof Error ? error.message : 'An error occurred'
