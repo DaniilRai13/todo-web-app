9fefc7aec1f4c54b834d6b100dacabb85f58ea72
DaniilRai13 <testnet02@mail.ru>
2025-02-17 15:56:27 +0300

    refactor: unimportant changes

--- todo-app/src/main.tsx
+++ todo-app/src/main.tsx
@@ -1,16 +1,16 @@
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './App.tsx'
 import Auth from './components/screens/Auth/Auth.tsx'
 import { pageRoutes } from './config/pageRoutes.ts'
 import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/store.ts'
 import AuthProvider from './provider/AuthProvider.tsx'
-import ModalProvider from './provider/ModalProvider.tsx'
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import ModalProvider from './provider/ModalProvider/ModalProvider.tsx'
+import { store } from './store/store.ts'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
