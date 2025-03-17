9ff2d0c7fb183e2cdaf004f2eee6ab8ad5954087
DaniilRai13 <testnet02@mail.ru>
2025-03-17 21:40:03 +0300

    refactor: change text in count block

--- todo-app/src/components/pages/Home/TasksStatistic/TasksStatistic.tsx
+++ todo-app/src/components/pages/Home/TasksStatistic/TasksStatistic.tsx
@@ -1,15 +1,20 @@
-import { CSSProperties, FC } from 'react'
-import { INavigateSideProps } from '../../../../config/pageRoutes'
-import { Icon } from '../../../../shared/LucidIcon'
-import styles from './TasksStatistic.module.scss'
+import { CSSProperties, FC } from 'react';
+import { INavigateSideProps } from '../../../../config/pageRoutes';
+import { Icon } from '../../../../shared/LucidIcon';
+import styles from './TasksStatistic.module.scss';
 
 interface ITasksStatProps {
-	title: string
-	iconName: INavigateSideProps['icon']
-	style?: CSSProperties | undefined
+	title: string;
+	iconName: INavigateSideProps['icon'];
+	count: number;
+	style?: CSSProperties | undefined;
 }
-const TasksStatistic: FC<ITasksStatProps> = ({ title, iconName, style = {} }) => {
-
+const TasksStatistic: FC<ITasksStatProps> = ({
+	title,
+	iconName,
+	count,
+	style = {},
+}) => {
 	return (
 		<div className={styles.statistic} style={style}>
 			<div className={styles.icon}>
@@ -17,10 +22,12 @@ const TasksStatistic: FC<ITasksStatProps> = ({ title, iconName, style = {} }) =>
 			</div>
 			<div className={styles.info}>
 				<h3 className={styles.status}>{title}</h3>
-				<h3 className={styles.count}>24 tasks</h3>
+				<h3 className={styles.count}>
+					{count} {count === 1 ? 'task' : 'tasks'}
+				</h3>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default TasksStatistic
+export default TasksStatistic;
