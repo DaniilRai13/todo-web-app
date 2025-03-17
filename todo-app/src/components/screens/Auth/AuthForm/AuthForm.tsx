3dec9b468dfc70536af9100826f7882a93de6274
DaniilRai13 <testnet02@mail.ru>
2025-03-17 21:15:13 +0300

    fix: use only mode name to display names in auth forms

--- todo-app/src/components/screens/Auth/AuthForm/AuthForm.tsx
+++ todo-app/src/components/screens/Auth/AuthForm/AuthForm.tsx
@@ -1,48 +1,55 @@
-import { FC } from 'react'
-import cn from 'classnames'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { NavLink } from 'react-router'
-import Button from '../../../../shared/Button/Button'
-import Field from '../../../../shared/form/Field'
-import styles from './AuthForm.module.scss'
-import { useActions } from '../../../../shared/hooks/useActions'
-import { IAuthData } from '../../../../interfaces/Auth.interface'
-import { useTypedSelector } from '../../../../shared/hooks/useTypedSelector'
+import cn from 'classnames';
+import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { NavLink } from 'react-router';
+import { pageRoutes } from '../../../../config/pageRoutes';
+import { IAuthData } from '../../../../interfaces/Auth.interface';
+import Button from '../../../../shared/Button/Button';
+import Field from '../../../../shared/form/Field';
+import { useActions } from '../../../../shared/hooks/useActions';
+import { useTypedSelector } from '../../../../shared/hooks/useTypedSelector';
+import styles from './AuthForm.module.scss';
 
-const AuthForm: FC<{ name: string, isPage: boolean }> = ({ name, isPage }) => {
-	const { register, handleSubmit, formState: { errors }, reset } = useForm<IAuthData>({
-		mode: 'onChange'
-	})
-	const { register: registerAction, login } = useActions()
-	const { isLoading } = useTypedSelector(({ user }) => user)
+const AuthForm: FC<{ mode: string }> = ({ mode }) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm<IAuthData>({
+		mode: 'onChange',
+	});
+	const { register: registerAction, login } = useActions();
+	const { isLoading } = useTypedSelector(({ user }) => user);
+	const isRegister = mode === 'register';
+	const inputClassName = isRegister ? styles.register : styles.login;
 
-	const inputClassName = isPage ? styles.register : styles.login
-
-	const onSubmit: SubmitHandler<IAuthData> = (data) => {
-		if (isPage) {
-			registerAction(data)
-			reset()
-		}
-		else login(data)
-		reset()
-	}
-
+	const onSubmit: SubmitHandler<IAuthData> = data => {
+		if (isRegister) {
+			registerAction(data);
+			reset();
+		} else login(data);
+		reset();
+	};
 
 	return (
 		<div className={styles.formWindow}>
 			<h1
 				className={cn(styles.title, {
-					[styles.register]: isPage,
-					[styles.login]: !isPage
-				})}>{name.toUpperCase()}</h1>
+					[styles.register]: isRegister,
+					[styles.login]: !isRegister,
+				})}
+			>
+				{mode.toUpperCase()}
+			</h1>
 			<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 				<Field
 					{...register('email', {
 						required: 'This field is required',
 						pattern: {
 							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-							message: 'Invalid email address'
-						}
+							message: 'Invalid email address',
+						},
 					})}
 					placeholder='Email'
 					className={inputClassName}
@@ -62,25 +69,26 @@ const AuthForm: FC<{ name: string, isPage: boolean }> = ({ name, isPage }) => {
 					error={errors.password}
 				/>
 				<Button
-					title={name.toUpperCase()}
-					classNames={isPage ? styles.btn_register : styles.btn_login}
+					title={mode.toUpperCase()}
+					classNames={isRegister ? styles.btn_register : styles.btn_login}
 					isLoading={isLoading}
 				/>
 			</form>
 			<footer className={styles.footer}>
-				{name === 'register'
-					? <div className={styles.footerText}>
+				{isRegister ? (
+					<div className={styles.footerText}>
 						Already have an account?
-						<NavLink to={'/auth'}>Login</NavLink>
+						<NavLink to={pageRoutes.auth}>Login</NavLink>
 					</div>
-					: <div className={styles.footerText}>
+				) : (
+					<div className={styles.footerText}>
 						Don't have an account yet?
-						<NavLink to={'register'}>Register</NavLink>
+						<NavLink to={pageRoutes.register}>Register</NavLink>
 					</div>
-				}
+				)}
 			</footer>
 		</div>
-	)
-}
+	);
+};
 
-export default AuthForm
+export default AuthForm;
