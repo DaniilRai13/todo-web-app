0f8b1bf7acfa89bc786c2a5cc19020ce5c13627a
DaniilRai13 <testnet02@mail.ru>
2025-03-17 21:12:31 +0300

    feat: add routes structure ( add separate route to register)

--- todo-app/src/main.tsx
+++ todo-app/src/main.tsx
@@ -1,38 +1,33 @@
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import App from './App.tsx'
-import Auth from './components/screens/Auth/Auth.tsx'
-import { pageRoutes } from './config/pageRoutes.ts'
-import './index.css'
-import AuthProvider from './provider/AuthProvider.tsx'
-import ModalProvider from './provider/ModalProvider/ModalProvider.tsx'
-import { store } from './store/store.ts'
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import App from './App.tsx';
+import Auth from './components/screens/Auth/Auth.tsx';
+import { pageRoutes } from './config/pageRoutes.ts';
+import './index.css';
+import AuthProvider from './provider/AuthProvider.tsx';
+import ModalProvider from './provider/ModalProvider/ModalProvider.tsx';
+import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <AuthProvider>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <ModalProvider>
-              <Routes>
-                <Route
-                  path={pageRoutes.home + '*'}
-                  element={<App />}
-                />
-                <Route
-                  path={pageRoutes.auth + '/*'}
-                  element={<Auth />}
-                />
-              </Routes>
-            </ModalProvider>
-          </LocalizationProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>,
-)
+	<StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<AuthProvider>
+					<LocalizationProvider dateAdapter={AdapterDayjs}>
+						<ModalProvider>
+							<Routes>
+								<Route path={pageRoutes.home + '*'} element={<App />} />
+								<Route path={pageRoutes.auth} element={<Auth />} />
+								<Route path={pageRoutes.register} element={<Auth />} />
+							</Routes>
+						</ModalProvider>
+					</LocalizationProvider>
+				</AuthProvider>
+			</BrowserRouter>
+		</Provider>
+	</StrictMode>
+);
