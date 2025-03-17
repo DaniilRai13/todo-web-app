d09381c4ab78cc88ee2f34ec3f9f8075ce377ab3
DaniilRai13 <testnet02@mail.ru>
2025-03-17 21:36:21 +0300

    feat: use generic to describe elements in options array

--- todo-app/src/shared/Select.tsx
+++ todo-app/src/shared/Select.tsx
@@ -1,33 +1,49 @@
-import { FormControl, InputLabel, MenuItem, Select as MuiSelect } from '@mui/material'
-import { FC } from 'react'
+import {
+	FormControl,
+	InputLabel,
+	MenuItem,
+	Select as MuiSelect,
+} from '@mui/material';
 
-interface ISelect {
-  title: string
-  options: string[]
-  value: string
-  classNames?: string
-  changeOptionValue: (option: string) => void
+interface ISelect<T> {
+	title: string;
+	options: T[];
+	value: T;
+	classNames?: string;
+	changeOptionValue: (option: T) => void;
 }
 
-const Select: FC<ISelect> = (({ title, options, value, changeOptionValue,classNames }) => {
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" className={classNames}>
-      <InputLabel id="demo-select-small-label">{title}</InputLabel>
-      <MuiSelect
-        labelId="demo-select-small-label"
-        id="demo-select-small"
-        value={value}
-        label={title}
-        onChange={(e) => {
-          changeOptionValue(e.target.value)
-        }}
-      >
-        {options.map((option) =>
-          <MenuItem key={option} value={option}>{option}</MenuItem>
-        )}
-      </MuiSelect>
-    </FormControl>
-  )
-})
+const Select = <T,>({
+	title,
+	options,
+	value,
+	changeOptionValue,
+	classNames,
+}: ISelect<T>) => {
+	return (
+		<FormControl
+			sx={{ m: 1, minWidth: 120 }}
+			size='small'
+			className={classNames}
+		>
+			<InputLabel id='demo-select-small-label'>{title}</InputLabel>
+			<MuiSelect
+				labelId='demo-select-small-label'
+				id='demo-select-small'
+				value={value}
+				label={title}
+				onChange={e => {
+					changeOptionValue(e.target.value as T);
+				}}
+			>
+				{options.map(option => (
+					<MenuItem key={String(option)} value={String(option)}>
+						{String(option)}
+					</MenuItem>
+				))}
+			</MuiSelect>
+		</FormControl>
+	);
+};
 
-export default Select
+export default Select;
