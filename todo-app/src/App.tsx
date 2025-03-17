2c1a6fa706a34d65c81091c193c5b05691a8faaa
DaniilRai13 <testnet02@mail.ru>
2025-03-17 21:13:28 +0300

    feat: use pageRoutes to define paths

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,55 +1,50 @@
-import { AnimatePresence } from 'motion/react'
-import { useEffect } from 'react'
-import { Navigate, Route, Routes } from 'react-router'
-import './App.scss'
-import Layout from './components/layout/Layout'
-import Calendar from './components/pages/Calendar/Calendar'
-import Profile from './components/pages/Profile/Profile'
-import Home from './components/pages/Home/Home'
-import { pageRoutes } from './config/pageRoutes'
-import { useActions } from './shared/hooks/useActions'
-import { useTypedSelector } from './shared/hooks/useTypedSelector'
-import Alert from './shared/notification/Alert'
-import { saveUserToStorage } from './store/user/user.localstorage'
-import Todos from './components/pages/Todos/Todos'
+import { AnimatePresence } from 'motion/react';
+import { useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+import './App.scss';
+import Layout from './components/layout/Layout';
+import Calendar from './components/pages/Calendar/Calendar';
+import Home from './components/pages/Home/Home';
+import Profile from './components/pages/Profile/Profile';
+import Todos from './components/pages/Todos/Todos';
+import { pageRoutes } from './config/pageRoutes';
+import { useActions } from './shared/hooks/useActions';
+import { useTypedSelector } from './shared/hooks/useTypedSelector';
+import Alert from './shared/notification/Alert';
+import { saveUserToStorage } from './store/user/user.localstorage';
 
 function App() {
-  const { user, isSuccess, error } = useTypedSelector(({ user }) => user)
-  const { resetStatus } = useActions()
+	const { user, isSuccess, error } = useTypedSelector(({ user }) => user);
+	const { resetStatus } = useActions();
 
-  useEffect(() => {
-    if (isSuccess && user) saveUserToStorage(user)
-    if (error || isSuccess) {
-      const timer = setTimeout(() => {
-        resetStatus()
-      }, 2600)
+	useEffect(() => {
+		if (isSuccess && user) saveUserToStorage(user);
+		if (error || isSuccess) {
+			const timer = setTimeout(() => {
+				resetStatus();
+			}, 2600);
 
-      return () => clearTimeout(timer)
-    }
-  }, [error, isSuccess, resetStatus, user])
+			return () => clearTimeout(timer);
+		}
+	}, [error, isSuccess, resetStatus, user]);
 
-  return (
-    <>
-      <Routes>
-        <Route
-          path={pageRoutes.home}
-          element={<Layout />}
-        >
-          <Route index element={<Navigate to={'/overview'} />} />
-          <Route path='overview' element={<Home />} />
-          <Route path='todo-list' element={<Todos />} />
-          <Route path='calendar' element={<Calendar />} />
-          <Route path='profile' element={<Profile />} />
-        </Route>
-      </Routes>
-      <AnimatePresence>
-        {error && <Alert type='error' message={error} />}
-        {isSuccess && <Alert type='success' />}
-      </AnimatePresence>
-    </>
-  )
+	return (
+		<>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route index element={<Navigate to='/overview' replace />} />
+					<Route path={pageRoutes.overview} element={<Home />} />
+					<Route path={pageRoutes.todo_list} element={<Todos />} />
+					<Route path={pageRoutes.calendar} element={<Calendar />} />
+					<Route path={pageRoutes.profile} element={<Profile />} />
+				</Route>
+			</Routes>
+			<AnimatePresence>
+				{error && <Alert type='error' message={error} />}
+				{isSuccess && <Alert type='success' />}
+			</AnimatePresence>
+		</>
+	);
 }
 
-
-
-export default App
+export default App;
