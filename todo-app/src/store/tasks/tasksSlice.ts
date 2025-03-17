656c570f67f7040441de4efc20bf3c319368f3b4
DaniilRai13 <testnet02@mail.ru>
2025-03-17 23:04:37 +0300

    fix: fix isLoading in getTasks "pending''

--- todo-app/src/store/tasks/tasksSlice.ts
+++ todo-app/src/store/tasks/tasksSlice.ts
@@ -1,53 +1,57 @@
-import { createSlice } from "@reduxjs/toolkit"
-import { ITask } from '../../config/user.data'
-import { createTask, getTasks } from './tasks.actions'
+import { createSlice } from '@reduxjs/toolkit';
+import { ITask } from '../../config/user.data';
+import { createTask, getTasks } from './tasks.actions';
 
 const initialState = {
-  tasks: [] as ITask[],
-  upcomingTasks: [] as ITask[],
-  isLoading: false,
-  isSuccess: false,
-  error: null as string | undefined | null,
-}
+	tasks: [] as ITask[],
+	upcomingTasks: [] as ITask[],
+	isLoading: false,
+	isSuccess: false,
+	error: null as string | undefined | null,
+};
 
 export const tasksSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    filterTasksByDate: (state, { payload }: { payload: ITask[] }) => {
-      state.upcomingTasks = payload.sort((a, b) => new Date(a.endDate).getTime() - new Date(b.endDate).getTime()).slice(0, 6)
-    }
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(createTask.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(createTask.fulfilled, (state, { payload }) => {
-        state.tasks.push(payload)
-        state.isSuccess = true
-        state.isLoading = false
-      })
-      .addCase(createTask.rejected, (state, { error }) => {
-        state.isSuccess = false
-        state.isLoading = false
-        state.error = error.message
-      })
-      .addCase(getTasks.pending, (state) => {
-        state.isLoading = false
-      })
-      .addCase(getTasks.fulfilled, (state, { payload }) => {
-        state.isSuccess = true
-        state.isLoading = false
-        state.tasks = payload
-      })
-      .addCase(getTasks.rejected, (state, { error }) => {
-        state.isSuccess = false
-        state.isLoading = false
-        state.error = error.message
-      })
-
-  }
-})
-export const { filterTasksByDate } = tasksSlice.actions
-export default tasksSlice.reducer
+	name: 'user',
+	initialState,
+	reducers: {
+		filterTasksByDate: (state, { payload }: { payload: ITask[] }) => {
+			state.upcomingTasks = payload
+				.sort(
+					(a, b) =>
+						new Date(a.endDate).getTime() - new Date(b.endDate).getTime()
+				)
+				.slice(0, 6);
+		},
+	},
+	extraReducers(builder) {
+		builder
+			.addCase(createTask.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(createTask.fulfilled, (state, { payload }) => {
+				state.tasks.push(payload);
+				state.isSuccess = true;
+				state.isLoading = false;
+			})
+			.addCase(createTask.rejected, (state, { error }) => {
+				state.isSuccess = false;
+				state.isLoading = false;
+				state.error = error.message;
+			})
+			.addCase(getTasks.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(getTasks.fulfilled, (state, { payload }) => {
+				state.isSuccess = true;
+				state.isLoading = false;
+				state.tasks = payload;
+			})
+			.addCase(getTasks.rejected, (state, { error }) => {
+				state.isSuccess = false;
+				state.isLoading = false;
+				state.error = error.message;
+			});
+	},
+});
+export const { filterTasksByDate } = tasksSlice.actions;
+export default tasksSlice.reducer;
