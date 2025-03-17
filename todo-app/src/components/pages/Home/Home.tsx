97f61d90bb29e9339c6635ae349cc07f6e7ec42f
DaniilRai13 <testnet02@mail.ru>
2025-03-17 23:07:30 +0300

    refactor: move TasksStatistic in separate file

--- todo-app/src/components/pages/Home/Home.tsx
+++ todo-app/src/components/pages/Home/Home.tsx
@@ -1,41 +1,29 @@
-import { FC, useEffect } from 'react'
-import { useActions } from '../../../shared/hooks/useActions'
-import { useTypedSelector } from '../../../shared/hooks/useTypedSelector'
-import styles from './Home.module.scss'
-import TasksStatistic from './TasksStatistic/TasksStatistic'
+import { FC, useEffect } from 'react';
+import { useActions } from '../../../shared/hooks/useActions';
+import { useTypedSelector } from '../../../shared/hooks/useTypedSelector';
+import styles from './Home.module.scss';
+
+import TaskStatistics from './TasksStatistics/TaskStatistics';
+import UpcomingTasks from './UpcomingTasks/UpcomingTasks';
 const Home: FC = () => {
-	const { user } = useTypedSelector(({ user }) => user)
-	const { getTasks } = useActions()
+	const { user } = useTypedSelector(({ user }) => user);
+
+	const { getTasks } = useActions();
 
 	useEffect(() => {
 		if (user) {
-			getTasks(user.id)
+			getTasks(user.id);
 		}
-	}, [getTasks, user])
+	}, [getTasks, user]);
 
 	return (
 		<div className={styles.home}>
 			<div className={styles.tasksInfoInner}>
-				<div className={styles.tasksInfo}>
-					<TasksStatistic
-						title='In process'
-						iconName='Hourglass'
-						style={{ background: 'rgb(243 215 35' }} />
-					<TasksStatistic
-						title='In pending'
-						iconName='Loader' />
-					<TasksStatistic
-						title='Completed'
-						iconName='BookmarkCheck'
-						style={{ background: '#77d69f' }} />
-					<TasksStatistic
-						title='Expired'
-						iconName='BookmarkCheck'
-						style={{ background: 'rgba(228, 80, 80, 0.7803921569)' }} />
-				</div>
+				<TaskStatistics />
 			</div>
+			<UpcomingTasks />
 		</div>
-	)
-}
+	);
+};
 
-export default Home
+export default Home;
