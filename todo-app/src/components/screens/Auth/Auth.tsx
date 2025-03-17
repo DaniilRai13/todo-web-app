f49c7e8a251a876ce1159d3cbb11493ef898ae64
DaniilRai13 <testnet02@mail.ru>
2025-03-17 21:14:32 +0300

    fix: remove routes to display AuthForm

--- todo-app/src/components/screens/Auth/Auth.tsx
+++ todo-app/src/components/screens/Auth/Auth.tsx
@@ -1,53 +1,54 @@
-import cn from 'classnames'
-import { AnimatePresence } from 'framer-motion'
-import { FC, useEffect } from 'react'
-import { Route, Routes, useLocation, useNavigate } from 'react-router'
-import { useActions } from '../../../shared/hooks/useActions'
-import { useTypedSelector } from '../../../shared/hooks/useTypedSelector'
-import Alert from '../../../shared/notification/Alert'
-import styles from './Auth.module.scss'
-import AuthForm from './AuthForm/AuthForm'
+import cn from 'classnames';
+import { AnimatePresence } from 'framer-motion';
+import { FC, useEffect } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router';
+import { pageRoutes } from '../../../config/pageRoutes';
+import { useActions } from '../../../shared/hooks/useActions';
+import { useTypedSelector } from '../../../shared/hooks/useTypedSelector';
+import Alert from '../../../shared/notification/Alert';
+import styles from './Auth.module.scss';
+import AuthForm from './AuthForm/AuthForm';
 
 const Auth: FC = () => {
-	const { pathname } = useLocation()
-	const { resetStatus } = useActions()
-	const isRegisterPage = pathname.includes('register')
-	const { error, isSuccess } = useTypedSelector(({ user }) => user)
-	const navigate = useNavigate()
+	const { pathname } = useLocation();
+	const { resetStatus } = useActions();
+	const isRegisterPage = pathname === pageRoutes.register;
+	
+	const { error, isSuccess } = useTypedSelector(({ user }) => user);
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		if (error || isSuccess) {
 			if (isSuccess && isRegisterPage) {
-				navigate('/auth')
+				navigate(pageRoutes.auth);
 			}
 			const timer = setTimeout(() => {
-				resetStatus()
-			}, 2600)
+				resetStatus();
+			}, 2600);
 
-			return () => clearTimeout(timer)
+			return () => clearTimeout(timer);
 		}
-	}, [error, isRegisterPage, isSuccess, navigate, resetStatus])
+	}, [error, isRegisterPage, isSuccess, navigate, resetStatus]);
 
 	return (
 		<div className={styles.container}>
-			<section className={cn(styles.left, {
-				[styles.register]: isRegisterPage,
-				[styles.login]: !isRegisterPage
-			})}>
+			<section
+				className={cn(styles.left, {
+					[styles.register]: isRegisterPage,
+					[styles.login]: !isRegisterPage,
+				})}
+			>
 				<h1>{isRegisterPage ? 'Register' : 'Login'}</h1>
 			</section>
 			<section className={styles.right}>
-				<Routes>
-					<Route index element={<AuthForm name='login' isPage={isRegisterPage} />} />
-					<Route path='/register' element={<AuthForm name='register' isPage={isRegisterPage} />} />
-				</Routes>
+				<AuthForm mode={isRegisterPage ? 'register' : 'login'} />
 			</section>
 			<AnimatePresence>
 				{error && <Alert type='error' message={error} />}
 				{isSuccess && <Alert type='success' />}
 			</AnimatePresence>
 		</div>
-	)
-}
+	);
+};
 
-export default Auth
+export default Auth;
