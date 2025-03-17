03048863f5792164ed7efef4a3d5bea5364f1827
DaniilRai13 <testnet02@mail.ru>
2025-03-17 23:04:56 +0300

    feat: add skeleton

--- todo-app/src/components/pages/Home/UpcomingTasks/UpcomingTasks.tsx
+++ todo-app/src/components/pages/Home/UpcomingTasks/UpcomingTasks.tsx
@@ -1,34 +1,44 @@
-import { FC, useState } from 'react'
-import Button from '../../../../shared/Button/Button'
-import Heading from '../../../../shared/Heading/Heading'
-import { useTypedSelector } from '../../../../shared/hooks/useTypedSelector'
-import UpcomingTask from './UpcomingTask/UpcomingTask'
-import styles from './UpcomingTasks.module.scss'
+import { FC, useState } from 'react';
+import Button from '../../../../shared/Button/Button';
+import Heading from '../../../../shared/Heading/Heading';
+import { useTypedSelector } from '../../../../shared/hooks/useTypedSelector';
+import SkeletonLoader from '../../../../shared/SkeletonLoader/SkeletonLoader';
+import UpcomingTask from './UpcomingTask/UpcomingTask';
+import styles from './UpcomingTasks.module.scss';
 const UpcomingTasks: FC = () => {
-	const { upcomingTasks } = useTypedSelector(({ tasks }) => tasks)
-	const [isShowMore, setIsShowMore] = useState(false)
-	const visibleTasks = upcomingTasks.length > 3 && isShowMore
-		? upcomingTasks
-		: upcomingTasks.slice(0, 3)
-
+	const { upcomingTasks, isLoading } = useTypedSelector(({ tasks }) => tasks);
+	const [isShowMore, setIsShowMore] = useState(false);
+	const visibleTasks =
+		upcomingTasks.length > 3 && isShowMore
+			? upcomingTasks
+			: upcomingTasks.slice(0, 3);
+	console.log(isLoading);
 	return (
 		<section className={styles.upcomingTasks}>
 			<Heading className={styles.title}>upcoming tasks</Heading>
 			<div className={styles.tasksWrapper}>
-				<div className={styles.tasks}>
-					{visibleTasks.map((task) => <UpcomingTask
-						key={task.uid}
-						task={task}
-					/>)}
-				</div>
+				{isLoading ? (
+					<SkeletonLoader
+						count={3}
+						className={styles.upcomingSkeletons}
+						height={300}
+					/>
+				) : (
+					<div className={styles.tasks}>
+						{visibleTasks.map(task => (
+							<UpcomingTask key={task.uid} task={task} />
+						))}
+					</div>
+				)}
 			</div>
 			<Button
 				title={isShowMore ? 'Show less' : 'Show more'}
 				classNames={styles.upcomingBtn}
 				onClick={() => setIsShowMore(!isShowMore)}
+				disabled={isLoading}
 			/>
 		</section>
-	)
-}
+	);
+};
 
-export default UpcomingTasks
+export default UpcomingTasks;
