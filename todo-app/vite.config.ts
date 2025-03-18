6bd9c0e1877fe5355662d0e3b5eb3537f245a29e
DaniilRai13 <testnet02@mail.ru>
2025-03-18 21:58:04 +0300

    feat: implement aliases

--- todo-app/vite.config.ts
+++ todo-app/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces'),
+      '@provider': path.resolve(__dirname, './src/provider'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@config': path.resolve(__dirname, './src/config'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@common': path.resolve(__dirname, './src/common'),
+		},
+	},
+});
